dd4825e834bd3413ee7fff047147281e8b79e6da
panda2pepsi <panda2pepsi@vip.qq.com>
2020-07-19 23:42:16 +0800

    add description

--- 0000_avatar.py
+++ 0000_avatar.py
@@ -1,17 +1,15 @@
+# -*-coding:utf-8-*-
+
+# 第 0000 题： 将你的 QQ 头像（或者微博头像）右上角加上红色的数字
+# 类似于微信未读信息数量那种提示效果。
+
 from PIL import Image, ImageDraw, ImageFont
 
 img_path = "E:\\Codes\\Python\\Internet_Projects\\Exercise Book\\source\\0000_2.jpg"
-# open image
 img01 = Image.open(img_path)
-# get the img width and height
 width, height = img01.size
-# set the font size, based on the image size
 text_size = int(width / 5)
-# set the text font
 text_font = ImageFont.truetype("C:\\Windows\\Fonts\\Arial.ttf", text_size)
-# start to draw sth on it
 draw_img01 = ImageDraw.Draw(img01)
-# add text on this image
 draw_img01.text((width * 0.8, height * 0.06), "1", "red", font=text_font)
-# call the image viewer
 img01.show()
